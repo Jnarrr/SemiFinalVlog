51519691e710f070c667bb6c49720bea811e4942
Jnarrr <lfebruary60@gmail.com>
2022-04-28 17:59:59 +0800

    views and urls

--- FoodWarehouseAPI/views.py
+++ FoodWarehouseAPI/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . serializers import FoodsSerializer
+from . models import Foods
+
+class Foodsviewset(viewsets.ModelViewSet):
+    queryset = Foods.objects.all().order_by('Mfd_id')
+    serializer_class = FoodsSerializer
 
 # Create your views here.
