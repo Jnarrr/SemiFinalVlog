9dcf49b0410da5d50206212454d5cddd00c7c445
Jnarrr <lfebruary60@gmail.com>
2022-04-28 17:43:34 +0800

    models

--- FoodWarehouseAPI/models.py
+++ FoodWarehouseAPI/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Foods(models.Model):
+    Mfd_id = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    UnitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    UnitsInStock = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.Name
